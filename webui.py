a14df427ce9f718a8bad2202a904eb721aa2ff29
Achillea <67188913+Gokaii@users.noreply.github.com>
2024-03-19 15:25:12 +0800

    Add files via upload

--- webui.py
+++ webui.py
@@ -7,12 +7,28 @@ import shutil
 import csv
 import pandas as pd
 import librosa
+import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
 dev_root='/mnt/disk_4t/public/guokai/dcase2020/dev_data'
+best_dict={
+    'valve':'G_x_2_max',
+    'fan':'D_lof',
+    'pump':'D_lof',
+    'slider':'G_x_2_min',
+    'ToyCar':'D_maha',
+    'ToyConveyor':'D_maha'
+}
+threshold = {
+    'valve':1,
+    'fan':1,
+    'pump':1,
+    'slider':1,
+    'ToyCar':1,
+    'ToyConveyor':1
+}
 
-
-def calc_melspecgram(aud):
+def calc_specgram(aud):
     # sr=None声音保持原采样频率， mono=False声音保持原通道数
     data, fs = librosa.load(aud, sr=None, mono=False)
     #data = aud[1]
@@ -29,6 +45,27 @@ def calc_melspecgram(aud):
     plt.xlabel('Time(s)')
     plt.title('Mel Spectrogram')
 
+    return plt
+
+def calc_melspecgram(aud):
+    data, fs = librosa.load(aud, sr=None, mono=False)
+    L = len(data)
+    print('Time:', L / fs)
+    # 0.025s
+    framelength = 0.025
+    # NFFT点数=0.025*fs
+    framesize = int(framelength * fs)
+    print("NFFT:", framesize)
+    #提取mel特征
+    mel_spect = librosa.feature.melspectrogram(data, sr=fs, n_fft=framesize)
+    #转化为log形式
+    mel_spect = librosa.power_to_db(mel_spect, ref=np.max)
+    print('plot: ',mel_spect.shape)
+    #画mel谱图
+    librosa.display.specshow(mel_spect, sr=fs, x_axis='time', y_axis='mel')
+    plt.ylabel('Mel Frequency')
+    plt.xlabel('Time(s)')
+    plt.title('Mel Spectrogram')
     return plt
 
 
@@ -72,10 +109,54 @@ def start_infer(card,mt,aud):
     clean_infer_path()
     
     infer_result=pd.read_csv('cache/infer_res.csv',index_col=False)
-    print(infer_result)
+    #print(infer_result)
     infer_result=infer_result.T
-    print(infer_result)
-    return status, infer_result
+    #print(infer_result)
+
+    # mel_spect = np.load('cache/recon.npy')
+    # librosa.display.specshow(mel_spect, sr=16000, x_axis='time', y_axis='mel')
+    # plt.ylabel('Mel Frequency')
+    # plt.xlabel('Time(s)')
+    # plt.title('Mel Spectrogram')
+
+    plt_re = recon_plt()
+    #plt_sub = subtract_plt()
+
+    best_metric = best_dict[mt]
+    thres = threshold[mt]
+    met = init_metrics()
+    metric2id = {m: meid for m, meid in zip(met, range(len(met)))}
+    best_metid = metric2id[best_metric]
+    score = infer_result[1][best_metid]
+    #print('inf',infer_result[1][best_metid])
+    if float(score) > float(thres):
+        decision = '异常'
+    else:
+        decision = '正常'
+    info = '检测结果为：'+decision+'\n最佳指标为: '+ best_metric+'\n判断阈值为: '+ str(thres)+'\n异常分数为: '+ score
+    return status, infer_result, plt_re, info
+
+def recon_plt():
+    print('111111111111111111111111111111')
+    mel_spect = np.load('cache/recon.npy')
+    
+    librosa.display.specshow(mel_spect, sr=16000, x_axis='time', y_axis='mel')
+    plt.ylabel('Mel Frequency')
+    plt.xlabel('Time(s)')
+    plt.title('Mel Spectrogram')
+    return plt
+
+def subtract_plt():
+    recon = np.load('cache/recon.npy')
+    inp = np.load('cache/in.npy')
+    res = np.subtract(inp,recon)
+    print('res', res)
+    
+    librosa.display.specshow(res, sr=16000, x_axis='time', y_axis='mel')
+    plt.ylabel('Mel Frequency')
+    plt.xlabel('Time(s)')
+    plt.title('Mel Spectrogram')
+    return plt
 
 
 
@@ -126,17 +207,28 @@ if __name__ == '__main__':
                         card = gr.Dropdown(cards,label='使用显卡',value=cards[0])
                         start_infer_btn=gr.Button('开始检测')
                         infer_status=gr.Textbox(label='检测状态')
-                        infer_result=gr.DataFrame(label='检测结果',interactive=False,headers=['Metrics','Scores'])
-
+                        with gr.Row():
+                            with gr.Column():
+                                mel_plot_recon = gr.Plot(label='重构梅尔谱')
+                            with gr.Column():
+                                mel_plot_sub = gr.Plot(label='异常可视化')
+                        with gr.Row():
+                            with gr.Column():
+                                infer_result=gr.DataFrame(label='异常分数',interactive=False,headers=['Metrics','Scores'])
+                            with gr.Column():
+                                decision=gr.Textbox(label='检测结果')
                 aud.change(fn=calc_melspecgram,
                                inputs=[aud],
                                outputs=[mel_plot])        
-            
+                infer_result.change(fn=subtract_plt,
+                                      inputs=[],
+                                      outputs=[mel_plot_sub])
+
                 start_infer_btn.click(fn=start_infer,
                                     inputs=[card,mt,aud], 
-                                    outputs=[infer_status,infer_result])
+                                    outputs=[infer_status,infer_result,mel_plot_recon, decision])
                         
     webbrowser.open("http://127.0.0.1:6677")
-    demo.launch(server_name='127.0.0.1', 
+    demo.launch(server_name='0.0.0.0', 
                 server_port=6677,
                 share=True)
